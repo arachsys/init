fbebfb6b6dbde49e2096a56a1c81808d896cc312
Chris Webb <chris@arachsys.com>
2022-11-28 12:28:06 +0000

    Remove subprocess support from uevent
    It is no harder to run uevent as a child of ueventd rather than as the
    parent, and this allows both a simpler uevent.c and a more logical
    process tree.
    
    Take care to kill the child correctly when terminating, but we keep it
    alive across a SIGHUP-triggered re-exec to avoid dropping uevents.

--- uevent.c
+++ uevent.c
@@ -1,7 +1,5 @@
 #include <err.h>
 #include <errno.h>
-#include <signal.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,63 +10,15 @@
 
 #define BUFFER 4096
 
-static pid_t pid;
-
-static void handler(int signal) {
-  if (signal != SIGPIPE && pid > 0)
-    kill(pid, signal);
-  if (signal == SIGPIPE || signal == SIGTERM)
-    _exit(EXIT_SUCCESS);
-}
-
-static void subprocess(char **argv) {
-  int eventpipe[2];
-
-  if (pipe(eventpipe) < 0)
-    err(EXIT_FAILURE, "pipe");
-
-  signal(SIGCHLD, SIG_IGN);
-  switch (pid = fork()) {
-    case -1:
-      err(EXIT_FAILURE, "fork");
-    case 0:
-      if (dup2(eventpipe[0], STDIN_FILENO) < 0)
-        err(EXIT_FAILURE, "dup2");
-      close(eventpipe[0]);
-      close(eventpipe[1]);
-      execvp(argv[0], argv);
-      err(EXIT_FAILURE, "exec");
-  }
-
-  if (dup2(eventpipe[1], STDOUT_FILENO) < 0)
-    err(EXIT_FAILURE, "dup2");
-  close(eventpipe[0]);
-  close(eventpipe[1]);
-
-  /* Pass on HUP, INT, TERM, USR1, USR2; exit on TERM or PIPE. */
-  signal(SIGHUP, handler);
-  signal(SIGINT, handler);
-  signal(SIGPIPE, handler);
-  signal(SIGTERM, handler);
-  signal(SIGUSR1, handler);
-  signal(SIGUSR2, handler);
-
-  argv[0] = NULL;
-}
-
 int main(int argc, char **argv) {
   char buffer[BUFFER + 1], *cursor, *separator;
   int sock, socksize = 1 << 21;
   ssize_t length;
-  struct sockaddr_nl addr;
 
-  if (argc > 1)
-    subprocess(argv + 1);
-
-  memset(&addr, 0, sizeof(addr));
-  addr.nl_family = AF_NETLINK;
-  addr.nl_pid = getpid();
-  addr.nl_groups = 1;
+  struct sockaddr_nl addr = {
+    .nl_family = AF_NETLINK,
+    .nl_groups = 1
+  };
 
   if ((sock = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT)) < 0)
     err(EXIT_FAILURE, "socket");

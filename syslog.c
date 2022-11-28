fcbd94c9647406a5dcb85db2a9bd32d487e7941d
Chris Webb <chris@arachsys.com>
2022-11-28 20:47:00 +0000

    Update syslog.c to use /dev/kmsg instead of /proc/kmsg
    The newer interface is cleaner and allows for code simplifications
    including a single static buffer shared between syslog and kmsg.

--- syslog.c
+++ syslog.c
@@ -27,7 +27,7 @@
 #endif
 #endif
 
-static char *zone;
+static char buffer[BUFFER + 1], *zone;
 
 static int syslog_date(char *line, struct tm *date) {
   char *cursor;
@@ -47,31 +47,43 @@ static int syslog_date(char *line, struct tm *date) {
 }
 
 static char *syslog_facility(int priority) {
-  char *facility;
-  int value;
-
-  facility = "unknown"; /* facility not found in facilitynames[] */
-  for (value = 0; facilitynames[value].c_val >= 0; value++)
-    if (facilitynames[value].c_val == (priority & LOG_FACMASK))
-      facility = facilitynames[value].c_name;
-  return facility;
+  for (size_t i = 0; facilitynames[i].c_val >= 0; i++)
+    if (facilitynames[i].c_val == (priority & LOG_FACMASK))
+      return facilitynames[i].c_name;
+  return "unknown"; /* facility not found in facilitynames[] */
 }
 
-static int syslog_priority(char *line, char **facility, int *level) {
-  int priority, start;
+static int syslog_priority(char *line, int *priority) {
+  int start = 0;
 
-  start = 0, sscanf(line, "<%d>%n", &priority, &start);
-  if (start > 0) {
-    *facility = syslog_facility(priority);
-    *level = LOG_PRI(priority);
-  }
-
+  sscanf(line, "<%d>%n", priority, &start);
   return start;
 }
 
-static void syslog_recv(int fd, char *data, size_t size) {
-  char *cursor, *facility;
-  int level, length;
+int syslog_open(void) {
+  int fd;
+  struct sockaddr_un addr = {
+    .sun_family = AF_UNIX,
+    .sun_path = "/dev/log"
+  };
+
+  if ((fd = socket(AF_UNIX, SOCK_DGRAM | SOCK_NONBLOCK, 0)) < 0)
+    err(EXIT_FAILURE, "socket");
+
+  unlink(addr.sun_path);
+  umask(0111); /* Syslog socket should be writeable by everyone. */
+  if (bind(fd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+    err(EXIT_FAILURE, "bind %s", addr.sun_path);
+
+  if (setsockopt(fd, SOL_SOCKET, SO_PASSCRED, &(int) { 1 },
+        sizeof(int)) < 0)
+    err(EXIT_FAILURE, "setsockopt SO_PASSCRED %s", addr.sun_path);
+  return fd;
+}
+
+static void syslog_recv(int fd) {
+  char *cursor;
+  int length, priority;
   struct iovec block;
   struct msghdr header;
   struct tm date;
@@ -81,8 +93,8 @@ static void syslog_recv(int fd, char *data, size_t size) {
     char data[CMSG_SPACE(sizeof(struct ucred))];
   } cmsg;
 
-  block.iov_base = data;
-  block.iov_len = size - 1;
+  block.iov_base = buffer;
+  block.iov_len = sizeof(buffer) - 1;
   header.msg_name = NULL;
   header.msg_namelen = 0;
   header.msg_iov = &block;
@@ -99,131 +111,90 @@ static void syslog_recv(int fd, char *data, size_t size) {
     if (cmsg.hdr.cmsg_type == SCM_CREDENTIALS)
       memcpy(&id, CMSG_DATA(&cmsg.hdr), sizeof(struct ucred));
 
-  for (cursor = data; cursor < data + length; cursor++)
+  for (cursor = buffer; cursor < buffer + length; cursor++)
     if (*cursor == 0 || *cursor == '\n')
       *cursor = 0;
     else if ((*cursor < 32 && *cursor != '\t') || *cursor == 127)
       *cursor = ' ';
-  data[length] = 0;
+  buffer[length] = 0;
 
-  cursor = data, facility = syslog_facility(LOG_USER), level = LOG_NOTICE;
-  while (cursor < data + length) {
-    cursor += syslog_priority(cursor, &facility, &level);
+  cursor = buffer;
+  priority = LOG_DAEMON | LOG_NOTICE;
+
+  while (cursor < buffer + length) {
+    cursor += syslog_priority(cursor, &priority);
     cursor += syslog_date(cursor, &date);
+
     if (*cursor) {
       if (zone && zone[0])
         printf("%u %u %u %s %u %04u-%02u-%02u %02u:%02u:%02u%c%02u%02u %s\n",
-          id.pid, id.uid, id.gid, facility, level, date.tm_year + 1900,
-          date.tm_mon + 1, date.tm_mday, date.tm_hour, date.tm_min,
-          date.tm_sec, date.tm_gmtoff < 0 ? '-' : '+',
-          abs((int) date.tm_gmtoff) / 3600,
+          id.pid, id.uid, id.gid, syslog_facility(priority),
+          priority & LOG_PRIMASK, date.tm_year + 1900, date.tm_mon + 1,
+          date.tm_mday, date.tm_hour, date.tm_min, date.tm_sec,
+          date.tm_gmtoff < 0 ? '-' : '+', abs((int) date.tm_gmtoff) / 3600,
           abs((int) date.tm_gmtoff) / 60 % 60, cursor);
       else
         printf("%u %u %u %s %u %04u-%02u-%02u %02u:%02u:%02u %s\n", id.pid,
-          id.uid, id.gid, facility, level, date.tm_year + 1900,
-          date.tm_mon + 1, date.tm_mday, date.tm_hour, date.tm_min,
-          date.tm_sec, cursor);
+          id.uid, id.gid, syslog_facility(priority), priority & LOG_PRIMASK,
+          date.tm_year + 1900, date.tm_mon + 1, date.tm_mday, date.tm_hour,
+          date.tm_min, date.tm_sec, cursor);
     }
     cursor += strlen(cursor) + 1;
   }
   fflush(stdout);
 }
 
-static int kernel_print(char *line) {
-  char *facility;
-  int level, start;
+static void kernel_read(int fd) {
+  char *cursor;
+  int length, priority;
   struct tm date;
   time_t now;
+
+  if ((length = read(fd, buffer, sizeof(buffer) - 1)) <= 0)
+    return;
 
   time(&now);
   (zone && zone[0] ? localtime_r : gmtime_r)(&now, &date);
 
-  facility = syslog_facility(LOG_KERN), level = LOG_NOTICE;
-  start = syslog_priority(line, &facility, &level);
-
-  if (line[start]) {
-    if (zone && zone[0])
-      printf("0 0 0 %s %u %04u-%02u-%02u %02u:%02u:%02u%c%02u%02u %s\n",
-        facility, level, date.tm_year + 1900, date.tm_mon + 1,
-        date.tm_mday, date.tm_hour, date.tm_min, date.tm_sec,
-        date.tm_gmtoff < 0 ? '-' : '+', abs((int) date.tm_gmtoff) / 3600,
-        abs((int) date.tm_gmtoff) / 60 % 60, line + start);
-    else
-      printf("0 0 0 %s %u %04u-%02u-%02u %02u:%02u:%02u %s\n", facility,
-        level, date.tm_year + 1900, date.tm_mon + 1, date.tm_mday,
-        date.tm_hour, date.tm_min, date.tm_sec, line + start);
-  }
-
-  return start;
-}
-
-static size_t kernel_read(int fd, char *data, size_t *length, size_t size) {
-  char *cursor;
-  ssize_t new;
-
-  if ((new = read(fd, data + *length, size - *length - 1)) <= 0)
-    return 0;
-
-  for (cursor = data + *length; cursor < data + *length + new; cursor++)
+  for (cursor = buffer; cursor < buffer + length; cursor++)
     if (*cursor == 0 || *cursor == '\n')
       *cursor = 0;
     else if ((*cursor < 32 && *cursor != '\t') || *cursor == 127)
       *cursor = ' ';
-  *length += new;
+  buffer[length] = 0;
 
-  while ((cursor = memchr(data, 0, *length))) {
-    kernel_print(data);
-    *length = data + *length - cursor - 1;
-    memmove(data, cursor + 1, *length);
+  priority = strtoul(buffer, &cursor, 10);
+  if (cursor == buffer)
+    priority = LOG_KERN | LOG_NOTICE;
+
+  cursor = strchr(buffer, ';');
+  cursor = cursor ? cursor + 1 : buffer;
+
+  if (*cursor) {
+    if (zone && zone[0])
+      printf("0 0 0 %s %u %04u-%02u-%02u %02u:%02u:%02u%c%02u%02u %s\n",
+        syslog_facility(priority), priority & LOG_PRIMASK,
+        date.tm_year + 1900, date.tm_mon + 1, date.tm_mday, date.tm_hour,
+        date.tm_min, date.tm_sec, date.tm_gmtoff < 0 ? '-' : '+',
+        abs((int) date.tm_gmtoff) / 3600,
+        abs((int) date.tm_gmtoff) / 60 % 60, cursor);
+    else
+      printf("0 0 0 %s %u %04u-%02u-%02u %02u:%02u:%02u %s\n",
+        syslog_facility(priority), priority & LOG_PRIMASK,
+        date.tm_year + 1900, date.tm_mon + 1, date.tm_mday, date.tm_hour,
+        date.tm_min, date.tm_sec, cursor);
+    fflush(stdout);
   }
-
-  if (*length >= size - 1) {
-    data[size - 1] = 0;
-    *length = kernel_print(data);
-  }
-
-  fflush(stdout);
-  return new;
 }
 
 int main(int argc, char **argv) {
   struct pollfd fds[2];
-  struct sockaddr_un addr = {
-    .sun_family = AF_UNIX,
-    .sun_path = "/dev/log"
-  };
 
-  struct {
-    char *data;
-    size_t length, size;
-  } kernel;
-
-  struct {
-    char *data;
-    size_t size;
-  } syslog;
+  if ((fds[0].fd = open("/dev/kmsg", O_RDONLY | O_NONBLOCK)) < 0)
+    err(EXIT_FAILURE, "open /dev/kmsg");
+  fds[1].fd = syslog_open();
 
   zone = getenv("TZ");
-
-  if (!(kernel.data = malloc(kernel.size = BUFFER + 1)))
-    err(EXIT_FAILURE, "malloc");
-  if (!(syslog.data = malloc(syslog.size = BUFFER + 1)))
-    err(EXIT_FAILURE, "malloc");
-
-  if ((fds[0].fd = open("/proc/kmsg", O_RDONLY)) < 0)
-    err(EXIT_FAILURE, "open /proc/kmsg");
-  kernel.length = 0;
-
-  if ((fds[1].fd = socket(AF_UNIX, SOCK_DGRAM, 0)) < 0)
-    err(EXIT_FAILURE, "socket");
-  unlink(addr.sun_path);
-  umask(0111); /* Syslog socket should be writeable by everyone. */
-  if (bind(fds[1].fd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
-    err(EXIT_FAILURE, "bind %s", addr.sun_path);
-
-  if (setsockopt(fds[1].fd, SOL_SOCKET, SO_PASSCRED, &(int) { 1 },
-        sizeof(int)) < 0)
-    err(EXIT_FAILURE, "setsockopt SO_PASSCRED");
 
   fds[0].events = fds[1].events = POLLIN;
   while(1) {
@@ -231,11 +202,8 @@ int main(int argc, char **argv) {
       if (errno != EAGAIN && errno != EINTR)
         err(EXIT_FAILURE, "poll");
     if (fds[0].revents & POLLIN)
-      if (!kernel_read(fds[0].fd, kernel.data, &kernel.length, kernel.size))
-        fds[0].events = 0;
+      kernel_read(fds[0].fd);
     if (fds[1].revents & POLLIN)
-      syslog_recv(fds[1].fd, syslog.data, syslog.size);
+      syslog_recv(fds[1].fd);
   }
-
-  return EXIT_FAILURE;
 }
